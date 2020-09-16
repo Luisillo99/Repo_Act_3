e22bb65d7d597e1392311e7b1be99f7cf4c127ca
Luis Preciado <luis.preciado3077@alumnos.udg.mx>
2020-09-15 23:29:03 -0500

    Se implementó un sistema de cuantificar y ordenar los sabores del usuario

--- Act3.py
+++ Act3.py
@@ -1,9 +1,21 @@
 #Actividad 3 - Sabores de Helado - Luis Enrique Preciado Muñiz
 
 sabores=["Chocolate","Vainilla","Fresa","Pistache","Napolitano"]
+gusto=[]
 nombre=input("Ingresa tu nombre: ")
 apell=input("Ingresa tu apellido: ")
+print("Del 1 al 10 indica que tanto te gusta cada sabor")
 for i in range(5):
-    print(f"{i+1}.-{sabores[i]}")
-sabor=int(input("Ingresa el n° de sabor de helado que más te guste: "))
-print(f"{nombre} {apell} el helado que mas te gusta es el de {sabores[sabor-1]}")
+    gusto.append(int(input(f"{sabores[i]}: ")))
+print(f"{nombre} {apell} los sabores que más te gustan ordenados de mayor a menor son: ")
+for i in range(5):
+    for j in range(i+1,5):
+        if gusto[i]<gusto[j]:
+            x=sabores[i]
+            y=gusto[i]
+            sabores[i]=sabores[j]
+            gusto[i]=gusto[j]
+            sabores[j]=x
+            gusto[j]=y
+for i in range(5):
+    print(f"{i+1}.- {sabores[i]}")
